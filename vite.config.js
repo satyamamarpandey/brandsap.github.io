350229058765a98d85f3dab5f4f5525447b383a7
satyamamarpandey <sp2943@njit.edu>
2025-12-30 15:17:12 -0500

    Fix Vite base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  // For a user/organization site like https://brandsap.github.io
-  // base should be "/" (root). This also works fine on localhost.
   base: "/",
   server: {
     port: 5173,
